04a9eed94ec06cea9a286db4614c1ae9ad145ae6
SHsus1122 <tjgurtn1122@naver.com>
2024-03-10 12:54:45 +0900

    base 키워드 연습

--- CSharp/OOP01.cs
+++ CSharp/OOP01.cs
@@ -8,19 +8,64 @@ using System.Threading.Tasks;
 namespace CSharp
 {
     // 객체지향(OOP, Object Oriented Programming)
+    // - 은닉성, 상속성, 다형성
+
+    class Player    // 부모 클래스 혹인 기반 클래스
+    {
+        static public int counter;  // 오로지 1 개만 존재합니다.
+        public int id;              // 플레이어 고유 아이디
+        public int hp;
+        public int attack;
+
+        public Player()
+        {
+            Console.WriteLine("Player 생성자 호출!");
+        }
+
+        public Player(int hp)
+        {
+            this.hp = hp;
+            Console.WriteLine("Player [HP] 생성자 호출!");
+        }
+
+        public Player(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+            Console.WriteLine("Player [HP, Attack] 생성자 호출!");
+        }
+    }
+
+    class Mage : Player // 자식 클래스 혹은 파생 클래스
+    {
+        
+    }
+
+    class Archer : Player
+    {
+        
+    }
 
     // Knight - 속성 : hp, attack / 기능 : Move, Attack, Die
     // 속성과 기능을 채워넣기
     // Class 의 경우 참조를 해서 값을 넘깁니다.
-    class Knight
+    class Knight : Player
     {
-        // Field(필드)
-        // 클래스 외부에서도 사용해주기 위해서 접근제어자 지정
-        static public int counter;  // 오로지 1 개만 존재합니다.
+        int myHp;
+        // 생성자
+        public Knight() : base(100, 10) // 부모의 멤버 선택 키워드 "base"
+        {
+            id = counter;
+            counter++;
 
-        public int id;              // 플레이어 고유 아이디
-        public int hp;
-        public int attack;
+            hp = 100;
+            attack = 10;
+
+            this.myHp = 10; // 자식의 멤버 변수 접근
+            base.hp = hp;   // 부모의 멤버 변수 접근
+
+            Console.WriteLine("Knight 생성자 호출!");
+        }
 
         // Knight 생성용 정적 함수
         static public Knight CreateKnight()
@@ -29,17 +74,6 @@ namespace CSharp
             Knight.hp = 100;
             Knight.attack = 10;
             return Knight;
-        }
-
-        // 생성자
-        public Knight()
-        {
-            id = counter;
-            counter++;
-
-            hp = 100;
-            attack = 10;
-            Console.WriteLine("생성자 호출!");
         }
 
         // 이동 기능 함수
