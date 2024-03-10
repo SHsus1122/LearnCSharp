7e600626ee1bb11134f317e8fc5626c3470bbba2
SHsus1122 <tjgurtn1122@naver.com>
2024-03-10 16:26:35 +0900

    is, as 키워드 사용 및 형변환 연습

--- CSharp/OOP01.cs
+++ CSharp/OOP01.cs
@@ -10,68 +10,66 @@ namespace CSharp
     // 객체지향(OOP, Object Oriented Programming)
     // - 은닉성, 상속성, 다형성
 
-    // 자동차를 탄다고 가정 합니다.
-    // 핸들, 폐달, 차문 같은 외부로 노출되는 기능이 있고
-    // 전기장치, 엔진 같은 외부로 노출되지 않는 기능이 있다고 합니다.
-    //  여기서 은닉성은 위의 기능들 중 노출되면 않되는 중요 기능들을 숨기는 것들을 말합니다.
-
-    class Knight
+    class Player
     {
-        // 접근 제어자(제한 레벨) public, protected, private
-        //  - pulic     : 모든 곳에서 사용 가능
-        //  - private   : 본인 클래스 내에서만 사용 가능
-        //  - protected : 자식에서만 접근 가능
-        private int hp;
-        public int mp;
-        protected int ap;
-        
-        // 외부에서 접근 불가
-        public void SetHp(int hp)
-        {
-            this.hp = hp;
-        }
-
-        public int GetHp()
-        {
-            return this.hp;
-        }
+        protected int hp;
+        protected int attack;
     }
 
-    class SuperKnight : Knight
+    class Knight : Player
     {
-        // private 레벨인 hp 는 에러 발생
-        public void SetMp()
-        {
-            this.hp = 150;
-        }
 
-        // protected 레벨인 ap 는 정상 작동
-        public void SetAp()
-        {
-            this.ap = 50;
-        }
+    }
+
+    class Mage : Player
+    {
+        public int mp;
     }
 
     internal class OOP01
     {
+        // "is" 키워드 사용한 형변환
+        /*        static void EnterGame(Player player)
+                {
+                    // 그래서 이렇게 "is" 를 활용해서 한 번 검증을 거쳐주면 해결할 수 있습니다.(안전한 형변환)
+                    bool isMage = (player is Mage);
+                    if (isMage)
+                    {
+                        Mage mage = (Mage)player;
+                        mage.mp = 10;
+                    }
+                }*/
+
+        // "as" 키워드 사용한 형변환
+        static void EnterGame(Player player)
+        {
+            // "as" 사용시 이렇게 리턴 값을 캐스팅할 타입까지 지정 가능, 실패시 null
+            Mage mage = (player as Mage);
+            if (mage != null)
+            {
+                mage.mp = 10;
+            }
+        }
+
         static void Main(string[] args)
         {
             Knight knight = new Knight();
+            Mage mage = new Mage();
 
-            // private 레벨인 hp 와 protected 인 ap 는 직접 접근 불가능, 에러 발생
-            knight.hp = 100;
-            knight.ap = 100;
-            // 하지만, public 의 경우 이렇게 직접 접근이 가능
-            knight.mp = 100;
+            /*
+                        // Mage -> 타입 Player 타입 변환 가능
+                        // Player 타입 -> Mage 타입 변환 불가능
+                        Player magePlayer = mage;
+                        // 하지만 (Mage) 를 사용해서 직접 명시해주면 형변환은 가능
+                        // 다만, 아래 방식은 강제로 형변환 한 것이기 때문에 문제가 생길 수 있습니다.
+                        Mage mage2 = (Mage)magePlayer;
 
-            // 하지만 아래처럼 Getter, Setter 를 활용한 내부 함수로는 가능
-            knight.SetHp(100);                  // 100 으로 설정
-            Console.WriteLine(knight.GetHp());  // 결과 : 100
+                        // 즉, 자식에서 부모로 가는 것은 문제가 없습니다.
+                        // 하지만, 부모에서 자식으로 가는 것은 문제가 생길 수도 있고 안 생길 수도 있습니다.(Case by Case)
+            */
 
-            SuperKnight superKnight = new SuperKnight();
-            // protected 레벨인 ap 는 자식에서 접근 가능하나 이는 어디까지나 상속받은 클래스 내에서 가능
-            // 이렇게 직접 접근은 불가능, 그래서 Getter, Setter 라는 함수(개념)가 필요합니다.
-            superKnight.ap = 100;
+            EnterGame(knight);
+            EnterGame(mage);
         }
     }
 }
