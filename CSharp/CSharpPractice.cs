9e48325e9956fc6a7e6f3df97dc8e4bcba5560ba
SHsus1122 <tjgurtn1122@naver.com>
2024-03-12 01:23:53 +0900

    Property 사용법 및 실습

--- CSharp/CSharpPractice.cs
+++ CSharp/CSharpPractice.cs
@@ -10,63 +10,37 @@ namespace CSharp
 {
     class CSharpPractice
     {
-        // 추상 클래스, abstract
-        abstract class Monsters
-        {
-            public abstract void Shout();
-        }
+        // 프로퍼티, Property
+        // 이를 위해서 객체지향의 은닉성에 대해서 다시 짚고 넘어갑니다.
 
-        // 인터페이스 사용
-        interface IFlyable
+        class Knights
         {
-            // 이 IFlyable 를 가진 인터페이스 에서는 Fly 라는 함수(기능)을 반드시 구현해야 합니다.
-            // 하지만 abstract 를 사용한 것과 달리 어떠한 형태로 구현할지는 강제하지 않습니다.
-            void Fly();
-        }
+            // 프로퍼티 선언
+            // 1. 일반적인 방법
+            /*protected int hp;
 
-        class Orc : Monsters
-        {
-            public override void Shout()
+            public int Hp
             {
-                Console.WriteLine("록타르 오가르!");
-            }
-        }
+                // value 는 default 명 입니다.
+                get { return hp; }
+                private set { hp = value; }
+            }*/
 
-        class Skeletons : Monsters
-        {
-            public override void Shout()
-            {
-                Console.WriteLine("꾸에에에엑!");
-            }
-        }
+            // 2. 자동 구현 프로퍼티 방법
+            public int Hp { get; set; } = 100;  // C# 7.0 부터 지원하는 바로 초기화 하는 문법(= 100;)
 
-        // Fly 기능을 반드시(기본적으로) 내포하고 있는 FlyableOrc 클래스
-        class FlyableOrc : Orc, IFlyable
-        {
-            public void Fly()
-            {
-
-            }
-        }
-
-        static void DoFly(IFlyable flyable)
-        {
-            // IFlyable 를 내포하고 있는 녀석은 DoFly 의 호출이 가능합니다 !
-
-            // 또한 이렇게 IFlyable 이 Fly 변수를 가지고 있기 때문에 이렇게 호출도 가능합니다 !
-            flyable.Fly();
+            private int hp;
+            public int GetHp() { return hp; }
+            public void SetHp(int value) { hp = value; } 
         }
 
         static void Main(string[] args)
         {
-            // 인터페이스 변수인 flyable 변수에 FlyableOrc 를 저장하는 것도 가능합니다.
-            // 역으로 하는 행우지만, 이런것이 인터페이스를 사용했을 때 가능합니다.
-            IFlyable flyable = new FlyableOrc();
-            DoFly(flyable);     // 인자로 전달이 가능함 !! 이런식으로 사용이 가능합니다.
+            Knights knights = new Knights();
 
-            // 당연히 이렇게 오크 자체를(FlyableOrc) 사용해서도 가능합니다. 
-            FlyableOrc orc = new FlyableOrc();
-            DoFly(orc);
+            // 프로퍼티 사용
+            knights.Hp = 100;
+            int hp = knights.Hp;
         }
     }
 }
